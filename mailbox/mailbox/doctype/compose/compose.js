f136b928fae71e1046e6a41cb9228fce03332cce
pawaranand <pawaranand00@gmail.com>
2015-07-08 18:15:55 +0530

    [Feature]Compose Mail

--- mailbox/mailbox/doctype/compose/compose.js
+++ mailbox/mailbox/doctype/compose/compose.js
@@ -1,63 +1,139 @@
-frappe.ui.form.on("Compose", {
-	refresh: function(frm) {
-		fields  = ["subject","content","recipients"]
-		$.each(fields,function(i,field){
-			frm.set_value(field,"");	
-		})
-		//frm.refresh()
-		frm.save()
+frappe.views.Composer = Class.extend({
+	init: function(opts) {
+		$.extend(this, opts);
+		this.make();
 	},
-	recipients:function(frm){
+	make: function() {
 		var me = this;
+		this.dialog = new frappe.ui.Dialog({
+			title: __("Add Reply") + ": " + (this.subject || ""),
+			no_submit_on_enter: true,
+			fields: [
+				{label:__("To"), fieldtype:"Data", reqd: 1, fieldname:"recipients"},
 
-		function split( val ) {
-			return val.split( /,\s*/ );
-		}
-		function extractLast( term ) {
-			return split(term).pop();
-		}
+				{fieldtype: "Section Break"},
+				{fieldtype: "Column Break"},
+				{label:__("Subject"), fieldtype:"Data", reqd: 1,
+					fieldname:"subject"},
+				{fieldtype: "Column Break"},
+				
+				{fieldtype: "Section Break"},
+				{label:__("Message"), fieldtype:"Text Editor", reqd: 1,
+					fieldname:"content"},
 
-		$(frm.doc.recipients.input)
-			.bind( "keydown", function(event) {
-				if (event.keyCode === $.ui.keyCode.TAB &&
-						$(this).data( "autocomplete" ) &&
-						$(this).data( "autocomplete" ).menu.active ) {
-					event.preventDefault();
+				{fieldtype: "Section Break"},
+				{fieldtype: "Column Break"},
+				{label:__("Send As Email"), fieldtype:"Check",
+					fieldname:"send_email"},
+					
+				{fieldtype: "Column Break"},
+				{label:__("Select Attachments"), fieldtype:"HTML",
+					fieldname:"select_attachments"}
+			],
+			primary_action_label: "Send",
+			primary_action: function() {
+				me.send_action();
+			}
+		});
+			
+
+		/*$(document).on("upload_complete", function(event, attachment) {
+			if(me.dialog.display) {
+				var wrapper = $(me.dialog.fields_dict.select_attachments.wrapper);
+
+				// find already checked items
+				var checked_items = wrapper.find('[data-file-name]:checked').map(function() {
+					return $(this).attr("data-file-name");
+				});
+
+				// reset attachment list
+				me.setup_attach();
+
+				// check latest added
+				checked_items.push(attachment.file_name);
+
+				$.each(checked_items, function(i, filename) {
+					wrapper.find('[data-file-name="'+ filename +'"]').prop("checked", true);
+				});
+			}
+		})*/
+		
+		this.prepare();
+		this.dialog.show();
+		var me = this;
+		var fields = this.dialog.fields_dict;
+		var attach = $(fields.select_attachments.wrapper);
+		$('<ul class="list-unstyled sidebar-menu form-attachments">\
+			<li class="divider"></li> <li class="h6 attachments-label">Attachments</li>\
+			<li class="divider"></li> <li><a class="strong add-attachment">Attach File \
+			<i class="octicon octicon-plus" style="margin-left: 2px;"></i></a></li></ul>').appendTo(attach)
+		
+
+		this.attachments = new frappe.ui.form.Attachment({
+			parent:attach,
+			frm:this.dialog
+			
+		});
+
+	},
+	prepare: function() {
+		var me = this;
+		//this.setup_email();
+		//this.setup_autosuggest();
+		$(this.dialog.fields_dict.recipients.input).val(this.recipients || "").change();
+		$(this.dialog.fields_dict.subject.input).val(this.subject || "").change();
+	},
+	send_action: function() {
+		var me = this,
+		form_values = me.dialog.get_values(),
+		btn = me.dialog.get_primary_btn();
+
+		if(!form_values) return;
+
+		/*var selected_attachments = $.map($(me.dialog.wrapper)
+			.find("[data-file-name]:checked"), function(element) {
+				return $(element).attr("data-file-name");
+			})*/
+
+		/*if(form_values.attach_document_print) {
+			if (cur_frm.print_preview.is_old_style(form_values.select_print_format || "")) {
+				cur_frm.print_preview.with_old_style({
+					format: form_values.select_print_format,
+					callback: function(print_html) {
+						me.send_email(btn, form_values, selected_attachments, print_html);
+					}
+				});
+			} else {
+				me.send_email(btn, form_values, selected_attachments, null, form_values.select_print_format || "");
+			}
+
+		} else {*/
+		me.send_email(btn, form_values);
+		//}
+	},
+
+	send_email: function(btn, form_values, selected_attachments, print_html, print_format) {
+		var me = this;
+		console.log(form_values.send_me_a_copy)
+		/*return frappe.call({
+			method:"mailbox.mailbox.doctype.inbox.inbox.make",
+			args: {
+				recipients: form_values.recipients,
+				subject: form_values.subject,
+				content: form_values.content,
+				send_email: form_values.send_email,
+			},
+			btn: btn,
+			callback: function(r) {
+				console.log(r)
+				if(!r.exc) {
+					if(form_values.send_email && r.message["recipients"])
+						msgprint(__("Email sent to {0}", [r.message["recipients"]]));
+					me.dialog.hide();
+				} else {
+					msgprint(__("There were errors while sending email. Please try again."));
 				}
-			})
-			.autocomplete({
-				source: function(request, response) {
-					return frappe.call({
-						method:'frappe.email.get_contact_list',
-						args: {
-							'select': "email_id",
-							'from': "Contact",
-							'where': "email_id",
-							'txt': extractLast(request.term).value || '%'
-						},
-						quiet: true,
-						callback: function(r) {
-							response($.ui.autocomplete.filter(
-								r.cl || [], extractLast(request.term)));
-						}
-					});
-				},
-				appendTo: frm.doc.recipients.input,
-				focus: function() {
-					// prevent value inserted on focus
-					return false;
-				},
-				select: function( event, ui ) {
-					var terms = split( this.value );
-					// remove the current input
-					terms.pop();
-					// add the selected item
-					terms.push( ui.item.value );
-					// add placeholder to get the comma-and-space at the end
-					terms.push( "" );
-					this.value = terms.join( ", " );
-					return false;
-				}
-			});
+			}
+		});*/
 	}
 });
--- mailbox/mailbox/doctype/inbox/inbox_list.js
+++ mailbox/mailbox/doctype/inbox/inbox_list.js
@@ -1,6 +1,11 @@
+{% include 'mailbox/mailbox/doctype/compose/compose.js' %};
+{% include 'mailbox/mailbox/doctype/compose/attachments.js' %};
 frappe.listview_settings["Inbox"] = {
 	onload: function(listview) {
-		listview.page.add_menu_item(__("Compose"), function() { listview.get_composer() });
+		listview.page.add_menu_item(__("Compose"), function() { 
+			new frappe.views.Composer({})
+ 
+		});
 	},
 	add_fields: ["tag"],
 	get_indicator: function(doc) {
